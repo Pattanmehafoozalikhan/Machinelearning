456583b54e11d2f92ae82db6956d5fdf82e7fe82
imrankhan417 <72134628+imrankhan417@users.noreply.github.com>
2021-10-02 21:43:18 +0530

    Update ml.py

--- ml.py
+++ ml.py
@@ -52,3 +52,19 @@ def main():
 
 if __name__ == "__main__":
 	main()
+
+def main():
+	# observations / data
+	x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+	y = np.array([1, 3, 2, 5, 7, 8, 8, 9, 10, 12])
+
+	# estimating coefficients
+	b = estimate_coef(x, y)
+	print("Estimated coefficients:\nb_0 = {} \
+		\nb_1 = {}".format(b[0], b[1]))
+
+	# plotting regression line
+	plot_regression_line(x, y, b)
+
+if __name__ == "__main__":
+	main()
